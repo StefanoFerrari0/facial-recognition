67679f4c1d0ba3d0a3a47dfc743aa329e8c86cfa
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2024-07-23 21:27:09 -0300

    fix production bugs

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000", "localhost:3000", "https://facial-recognition-lime.vercel.app/"],
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000", "localhost:3000", "https://facial-recognition-lime.vercel.app/", "*"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
